74794e75aca016035acb7dac468c390a94d4f773
Emre Öztürk <ozturkemry@gmail.com>
2020-05-07 15:08:50 +0300

    Project added

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {register} from 'ts-node';
+import {RegisterPayload} from './register-payload';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private url: 'http://localhost:8080/';
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+  register(registerPayload: RegisterPayload): Observable<any>{
+    return this.httpClient.post(this.url + 'signup', registerPayload);
+  }
+}
